9b4fc02dab96edc19a82f7c88022e51338dac462
Vitor Villar <vitorn@ciandt.com>
2015-11-23 17:11:10 -0200

    Validation of new connection form

--- events/new_connection_window_events.py
+++ events/new_connection_window_events.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from pprint import pprint
+from windows.message_box import MessageBox
+from model.connection import Connection
 
 
 class NewConnectionWindowEvents:
@@ -30,7 +31,88 @@ class NewConnectionWindowEvents:
             file_chooser.set_sensitive(False)
 
     def on_btn_save_clicked(self, btn):
-        pass
+        # First of all, validate the form
+        if self.validate_form():
+            # If OK, save the connection
+            self.save_connection()
 
     def on_btn_cancel_clicked(self, btn):
         self.window.destroy()
+
+    def validate_form(self):
+        # Get all object that need be validated
+        name = self.builder.get_object("txt_name")
+        host = self.builder.get_object("txt_host")
+        port = self.builder.get_object("txt_port")
+        user = self.builder.get_object("txt_user")
+        switch_key = self.builder.get_object("switch_use_key")
+        password = self.builder.get_object("txt_password")
+        confirm_password = self.builder.get_object("txt_password_confirm")
+        file_chooser = self.builder.get_object("filechooser_key")
+
+        # Check for the name
+        if len(name.get_text()) == 0:
+            MessageBox("Name can't be empty!")
+            return False
+
+        # Check for the host
+        if len(host.get_text()) == 0:
+            MessageBox("Host can't be empty!")
+            return False
+
+        # Check for the port
+        port_number = port.get_text()
+        if len(port_number) == 0:
+            MessageBox("Port can't be empty!")
+            return False
+        elif int(port_number) <= 0 or int(port_number) > 65565:
+            MessageBox("Port must be between 1 and 65565")
+            return False
+
+        # Check for the user
+        if len(user.get_text()) == 0:
+            MessageBox("User can't be empty!")
+            return False
+
+        # If switch key is On, the filechooser must have a file
+        if switch_key.get_active():
+            # If file chooser haven't any file, return error
+            if file_chooser.get_filename() is None:
+                MessageBox("You must select a key file")
+                return False
+        else:
+            # Check if password is empty
+            if len(password.get_text()) == 0:
+                MessageBox("Password can't be empty!")
+                return False
+
+            # Check if the password are the same, if not, return error
+            if password.get_text() != confirm_password.get_text():
+                MessageBox("The passwords not match")
+                return False
+
+        # If all pass, return True
+        return True
+
+    def save_connection(self):
+        # Get all object that need be validated
+        name = self.builder.get_object("txt_name")
+        host = self.builder.get_object("txt_host")
+        port = self.builder.get_object("txt_port")
+        user = self.builder.get_object("txt_user")
+        switch_key = self.builder.get_object("switch_use_key")
+        password = self.builder.get_object("txt_password")
+        file_chooser = self.builder.get_object("filechooser_key")
+
+        # Create a new Connection
+        connection = Connection()
+        connection.name = name.get_text()
+        connection.host = host.get_text()
+        connection.port = port.get_text()
+        connection.user = user.get_text()
+        connection.use_key = switch_key.get_active()
+        connection.key_path = file_chooser.get_filename()
+        connection.password = password.get_text()
+
+        # Save the connection
+        connection.save()
---
+++ windows/message_box.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from gi.repository import Gtk
+
+
+class MessageBox:
+    glade_file = "ui/message_box.glade"
+    main_object = None
+    handler_class = None
+    builder = None
+    window = None
+    text = None
+
+    def __init__(self, text):
+        # Set some properties
+        settings = Gtk.Settings.get_default()
+        settings.props.gtk_button_images = True
+        self.text = text
+
+        # Build the Window
+        self.build_window()
+
+        # Connect the signals
+        self.connect_events()
+
+    def build_window(self):
+        # Get the builder
+        self.builder = Gtk.Builder()
+
+        # inflate the layout
+        self.builder.add_from_file(self.glade_file)
+
+        self.window = self.builder.get_object("message_box")
+        self.window.set_markup(self.text)
+        self.window.connect("response", self.close_dialog)
+
+        self.window.show_all()
+
+    def connect_events(self):
+        # Connect the signals
+        self.handler_class = None
+        self.builder.connect_signals(self.handler_class)
+
+    def close_dialog(self, *args, **kwargs):
+        self.window.destroy()
+
