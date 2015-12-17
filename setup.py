cb18943552f77809d9ac41ab06d932a4fc1a013c
Vitor Villar <vitorn@ciandt.com>
2015-12-17 11:48:34 -0200

    Create the installer of the #2 issue

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@
 from distutils.core import setup
 
 setup(name='SSHClient',
-      version='1.0',
+      version='1.0.1',
       description='Manage SSH Connections via GUI',
       author='Vitor Villar',
       author_email='vitor.luis98@gmail.com',
       url='http://vitorluis.github.io/SSHClient/',
-      packages=['distutils', 'distutils.command'],
+      packages=['collection', 'data', 'events', 'model', 'tools', 'windows', 'ui'],
+      packages_data={
+          'data': ['data/sshclient.db'],
+          'ui': ['ui/about_window.glade', 'ui/connection_window.glade', 'ui/delete_connection_window.glade',
+                 'ui/file_export_dialog.glade', 'ui/main_window.glade', 'ui/message_box.glade',
+                 'ui/new_tunnel_window.glade', 'ui/settings_window.glade']
+      },
+      data_files=[('bitmaps', ['ui/images/delete.png', 'ui/images/edit.png,',
+                               'ui/images/new.png', 'ui/images/ssh.png'])],
       requires=['Crypto', 'gi']
       )
